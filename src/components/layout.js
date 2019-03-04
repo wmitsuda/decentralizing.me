aed30b4528a8b88bb9d4f10822356b1d19c8376c
Willian Mitsuda <wmitsuda@gmail.com>
2019-03-04 03:03:37 -0300

    Changing footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -64,9 +64,13 @@ class Layout extends React.Component {
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Willian Mitsuda - Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://www.gatsbyjs.org">Gatsby</a>, hosted in
+          {` `}
+          <a href="https://ipfs.io">IPFS</a> and served using
+          {` `}
+          <a href="http://cloudflare-ipfs.com">Cloudflare IPFS Gateway</a>
         </footer>
       </div>
     );
